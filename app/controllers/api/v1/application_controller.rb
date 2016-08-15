c1c9c1e04cc5b477194000f88e4aa0f064442244
Anselm Strauss <strauss@puzzle.ch>
2016-08-15 11:51:01 +0200

    eliminate common adapter

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,11 +1,23 @@
 module Api
+
   module V1
-    class ApplicationController < ::ApplicationController
+
+    class ApplicationController < ActionController::API
+
+      include ActionController::MimeResponds
+
+      before_action :require_json
+
+      def require_json
+        respond_to :json
+      end
 
       def ping
         render json: {text: 'pong'}
       end
 
     end
+
   end
+
 end
