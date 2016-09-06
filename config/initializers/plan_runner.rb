01356b8f1c509a669d057f57ad13ef14b7ff87b5
Anselm Strauss <strauss@puzzle.ch>
2016-09-06 16:54:46 +0200

    add init comment

--- config/initializers/plan_runner.rb
+++ config/initializers/plan_runner.rb
@@ -1,5 +1,8 @@
 require 'plan_runner'
 
+# Run plan runner, except:
+#   * when testing
+#   * running rake, such as 'rake db:migrate'
 unless (File.basename($0) == 'rake') or Rails.env.test?
   PlanRunner.instance
 end
