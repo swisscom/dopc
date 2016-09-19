54f4142874a2d8f6d50f8307f375f95a8a65a954
Anselm Strauss <strauss@puzzle.ch>
2016-09-19 15:49:40 +0200

    re-enabled tests

--- test/controllers/executions_controller_test.rb
+++ test/controllers/executions_controller_test.rb
@@ -23,32 +23,32 @@ class ExecutionsControllerTest < ActionDispatch::IntegrationTest
     assert_equal 4, data['id']
   end
 
-#  test 'start deploy' do
-#    post '/api/v1/executions', params: {plan: 'hello_world', task: 'deploy'}, as: :json
-#    data = JSON.parse(@response.body)
-#    assert_response :created
-#    assert_equal 4, data['id']
-#  end
-#
-#  test 'start run' do
-#    post '/api/v1/executions', params: {plan: 'hello_world', task: 'run'}, as: :json
-#    data = JSON.parse(@response.body)
-#    assert_response :created
-#    assert_equal 4, data['id']
-#  end
-#
-#  test 'start undeploy' do
-#    post '/api/v1/executions', params: {plan: 'hello_world', task: 'undeploy'}, as: :json
-#    data = JSON.parse(@response.body)
-#    assert_response :created
-#    assert_equal 4, data['id']
-#  end
-#
-#  test 'invalid task' do
-#    post '/api/v1/executions', params: {plan: 'hello_world', task: 'invalid'}, as: :json
-#    data = JSON.parse(@response.body)
-#    assert_response :unprocessable_entity
-#    assert_not_empty data['error']
-#  end
+  test 'start deploy' do
+    post '/api/v1/executions', params: {plan: 'hello_world', task: 'deploy'}, as: :json
+    data = JSON.parse(@response.body)
+    assert_response :created
+    assert_equal 4, data['id']
+  end
+
+  test 'start run' do
+    post '/api/v1/executions', params: {plan: 'hello_world', task: 'run'}, as: :json
+    data = JSON.parse(@response.body)
+    assert_response :created
+    assert_equal 4, data['id']
+  end
+
+  test 'start undeploy' do
+    post '/api/v1/executions', params: {plan: 'hello_world', task: 'undeploy'}, as: :json
+    data = JSON.parse(@response.body)
+    assert_response :created
+    assert_equal 4, data['id']
+  end
+
+  test 'invalid task' do
+    post '/api/v1/executions', params: {plan: 'hello_world', task: 'invalid'}, as: :json
+    data = JSON.parse(@response.body)
+    assert_response :unprocessable_entity
+    assert_not_empty data['error']
+  end
 
 end
