9921296f646c15bce30009e105dcb3b655189b0e
Anselm Strauss <strauss@puzzle.ch>
2016-09-23 09:04:10 +0200

    put mock classes into separate file

--- test/plan_worker_test.rb
+++ test/plan_worker_test.rb
@@ -1,30 +1,8 @@
 require 'test_helper'
 require 'plan_worker'
+require 'plan_execution_mock'
 
 class PlanWorkerTest < ActiveSupport::TestCase
-
-  class PlanExecutionMock
-    attr_accessor :has_run
-    def initialize
-      @has_run = false
-    end
-    def run
-      @has_run = true
-    end
-  end
-
-  class PlanExecutionFailMock
-    attr_accessor :has_run, :has_started
-    def initialize
-      @has_started = false
-      @has_run = false
-    end
-    def run
-      @has_started = true
-      raise 'test'
-      @has_run = true
-    end
-  end
 
   test 'next id' do
     PlanWorker.class_variable_set(:@@next_id, 0)
